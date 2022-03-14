1c0db4dba2eb7f49f9ac92fec7b1a23569b0a426
Reese Critchlow <reesecritchlow@gmail.com>
2022-03-13 22:19:39 -0700

    attempted to restructure axios post request

--- src/utilities/ClickupFunctions.js
+++ src/utilities/ClickupFunctions.js
@@ -1,12 +1,15 @@
 const axios = require('axios')
 
 export default async function getClickupAccessToken(code) {
+  const url = `https://api.clickup.com/api/v2/oauth/token?`
   try {
-    const response = await axios.post(
-      `https://api.clickup.com/api/v2/oauth/token?client_id=${process.env.REACT_APP_CLICKUP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLICKUP_CLIENT_SECRET}&code=${code}`,
-    )
+    const response = await axios.post(url, {
+      client_id: process.env.REACT_APP_CLICKUP_CLIENT_ID,
+      client_secret: process.env.REACT_APP_CLICKUP_CLIENT_ID,
+      code,
+    })
     console.log(response)
   } catch (err) {
-    console.error(err)
+    console.log(err)
   }
 }
