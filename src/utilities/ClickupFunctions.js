562f790241b0ba89c86458694ec09527b132a64f
Reese Critchlow <reesecritchlow@gmail.com>
2022-03-15 16:51:06 -0700

    Retooled proxy, added headers

--- src/utilities/ClickupFunctions.js
+++ src/utilities/ClickupFunctions.js
@@ -2,10 +2,14 @@ const axios = require('axios')
 
 // eslint-disable-next-line import/prefer-default-export
 export async function getClickupAccessToken(code) {
-  const url = `/api/v2/oauth/token?client_id=${process.env.REACT_APP_CLICKUP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLICKUP_CLIENT_ID}&code=${code}`
+  const headers = {
+    Origin: 'https://api.clickup.com',
+  }
+
+  const url = `https://api.clickup.com/api/v2/oauth/token?client_id=${process.env.REACT_APP_CLICKUP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLICKUP_CLIENT_ID}&code=${code}`
 
   try {
-    const response = await axios.post(url)
+    const response = await axios.post(url, headers)
     console.log(response)
   } catch (err) {
     console.log(err)
