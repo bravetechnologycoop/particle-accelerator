e099d9a823d10f6fdadcd915516a0579752cd858
Reese Critchlow <reesecritchlow@gmail.com>
2022-03-22 09:19:32 -0700

    added more functions to ClickupFunctions

--- src/utilities/ClickupFunctions.js
+++ src/utilities/ClickupFunctions.js
@@ -3,7 +3,6 @@ const axios = require('axios')
 // eslint-disable-next-line import/prefer-default-export
 export async function getClickupAccessToken(code) {
   const url = `${process.env.REACT_APP_CLICKUP_PROXY_BASE_URL}/v2/oauth/token?client_id=${process.env.REACT_APP_CLICKUP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLICKUP_CLIENT_SECRET}&code=${code}`
-
   try {
     console.log(`POST to ${url}`)
     const response = await axios.post(url)
@@ -17,7 +16,6 @@ export async function getClickupAccessToken(code) {
 
 export async function getClickupUserName(token) {
   const url = `${process.env.REACT_APP_CLICKUP_PROXY_BASE_URL}/v2/user`
-
   try {
     const response = await axios.get(url, {
       headers: {
@@ -34,7 +32,6 @@ export async function getClickupUserName(token) {
 
 export async function getClickupWorkspaces(token) {
   const url = `${process.env.REACT_APP_CLICKUP_PROXY_BASE_URL}/v2/team`
-
   try {
     const response = await axios.get(url, {
       headers: {
@@ -42,8 +39,103 @@ export async function getClickupWorkspaces(token) {
       },
     })
     console.log(response)
+    return response.teams.map(team => {
+      return { workspaceName: team.name, workspaceID: team.id }
+    })
   } catch (err) {
     console.error(err)
     return []
   }
+}
+
+export async function getClickupSpaces(token, workspaceID) {
+  const url = `${process.env.REACT_APP_CLICKUP_PROXY_BASE_URL}/v2/team/${workspaceID}/space`
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    console.log('spaces: ', response)
+    return response.spaces.map(space => {
+      return { spaceName: space.name, spaceID: space.id }
+    })
+  } catch (err) {
+    console.error(err)
+    return []
+  }
+}
+
+export async function getClickupSpaceFolders(token, spaceID) {
+  const url = `${process.env.REACT_APP_CLICKUP_PROXY_BASE_URL}/v2/space/${spaceID}/folder`
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    console.log('folders: ', response)
+    return response.folders.map(folder => {
+      return { folderName: folder.name, folderID: folder.id }
+    })
+  } catch (err) {
+    console.error(err)
+    return []
+  }
+}
+
+export async function getClickupListsInFolders(token, folderID) {
+  const url = `${process.env.REACT_APP_CLICKUP_PROXY_BASE_URL}/v2/folder/${folderID}/list`
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    console.log(`lists in folder ${folderID}: `, response)
+    return response.lists.map(list => {
+      return { listName: list.name, listID: list.id }
+    })
+  } catch (err) {
+    console.error(err)
+    return []
+  }
+}
+
+export async function getClickupListsWithoutFolders(token, spaceID) {
+  const url = `${process.env.REACT_APP_CLICKUP_PROXY_BASE_URL}/v2/space/${spaceID}/list`
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    console.log(`lists in space ${spaceID}: `, response)
+    return response.lists.map(list => {
+      return { listName: list.name, listID: list.id }
+    })
+  } catch (err) {
+    console.error(err)
+    return []
+  }
+}
+
+export async function getAllClickupListsInSpace(token, spaceID) {
+  const folders = await getClickupSpaceFolders(token, spaceID)
+  const allLists = []
+  if (folders !== []) {
+    for (const folder of folders) {
+      const listsInFolder = await getClickupListsInFolders(token, folder.folderID)
+      const taggedListsInFolder = listsInFolder.map(list => {
+        return { folderName: folder.folderName, listName: list.listName, listID: list.listID }
+      })
+      allLists.push(taggedListsInFolder)
+    }
+  }
+  const listsWithoutFolders = await getClickupListsWithoutFolders(token, spaceID)
+  const taggedListsWithoutFolders = listsWithoutFolders.map(list => {
+    return { folderName: '', listName: list.listName, listID: list.listID }
+  })
+  allLists.push(taggedListsWithoutFolders)
+  return allLists
 }
