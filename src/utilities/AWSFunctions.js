e75d9bcf7311dbf3a74f656a3a0f04f46010a2ba
Reese Critchlow <reesecritchlow@gmail.com>
2022-04-08 11:29:13 -0700

    added aws functionality with chatbot-dev backend integration

--- src/utilities/AWSFunctions.js
+++ src/utilities/AWSFunctions.js
@@ -1,55 +1,24 @@
 import axios from 'axios'
-// import { aws4Interceptor } from 'aws4-axios'
 
-const awsRegion = process.env.REACT_APP_AWS_REGION
-const awsService = 'iotwireless'
-const awsDeviceDestination = process.env.REACT_APP_AWS_DEVICE_DESTINATION
-const awsDeviceType = 'LoRaWAN'
-const awsAppEUI = process.env.REACT_APP_AWS_APP_EUI
-const awsAppKeyPostfix = process.env.REACT_APP_AWS_APP_KEY_POSTFIX
-
-const awsAccessKeyID = process.env.REACT_APP_AWS_ACCESS_KEY_DEV
-const awsSecretKey = process.env.REACT_APP_AWS_SECRET_KEY_DEV
-const awsDeviceProfileID = process.env.REACT_APP_AWS_DEVICE_PROFILE_ID_DEV
-const awsServiceProfileID = process.env.REACT_APP_AWS_SERVICE_PROFILE_ID_DEV
+const chatbotDevURL = process.env.REACT_APP_CHATBOT_DEV_URL
+const braveAPIKey = process.env.REACT_APP_BRAVE_API_KEY
 
 // eslint-disable-next-line import/prefer-default-export
-export function registerLoraButton(deviceEUI, targetName) {
-  /* const awsInterceptor = aws4Interceptor(
-    {
-      region: awsRegion,
-      service: awsService,
-    },
-    {
-      accessKeyId: awsAccessKeyID,
-      secretAccessKey: awsSecretKey,
-    },
-  ) */
-  const awsClient = axios.create()
-
-  // awsClient.interceptors.request.use(awsInterceptor)
-
-  const url = 'https://api.iotwireless.us-east-1.amazonaws.com/wireless-devices'
+export async function registerLoraButton(deviceEUI, targetName, clickupToken) {
+  const url = `${chatbotDevURL}/aws-device-registration`
 
   const data = {
-    DestinationName: awsDeviceDestination,
-    LoRaWAN: {
-      DevEUI: deviceEUI,
-      OtaaV1_0_x: {
-        AppEui: awsAppEUI,
-        AppKey: `${deviceEUI}${awsAppKeyPostfix}`,
-      },
-      ServiceProfileId: awsServiceProfileID,
-      DeviceProfileId: awsDeviceProfileID,
-    },
-    Name: targetName,
-    Type: awsDeviceType,
+    deviceEUI,
+    targetName,
+    clickupToken,
+    braveKey: braveAPIKey,
   }
 
   try {
-    const response = awsClient.post(url, data)
-    console.log(response)
+    const response = await axios.post(url, data)
+    return 'success'
   } catch (err) {
     console.error(err)
+    return err.response.data
   }
 }
--- src/views/ButtonRegistrationView.jsx
+++ src/views/ButtonRegistrationView.jsx
@@ -1,32 +1,71 @@
 import React, { useState } from 'react'
-import { Form } from 'react-bootstrap'
+import { Badge, Form, Spinner } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
+import PropTypes from 'prop-types'
 import { registerLoraButton } from '../utilities/AWSFunctions'
 
-export default function ButtonRegistrationView() {
+export default function ButtonRegistrationView(props) {
+  const { clickupToken } = props
+
   const [deviceEUI, setDeviceEUI] = useState('')
   const [deviceName, setDeviceName] = useState('')
+  const [formLock, setFormLock] = useState(false)
+  const [registrationStatus, setRegistrationStatus] = useState('idle')
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault()
-    setDeviceEUI('')
+    setRegistrationStatus('waiting')
+    setFormLock(true)
+    const awsRegistration = await registerLoraButton(deviceEUI, deviceName, clickupToken)
+
+    if (awsRegistration === 'success') {
+      setRegistrationStatus(awsRegistration)
+    } else {
+      setRegistrationStatus(awsRegistration)
+    }
+
     setDeviceName('')
-    registerLoraButton(deviceEUI, deviceName)
+    setFormLock(false)
   }
 
   return (
     <Form onSubmit={handleSubmit} style={{ maxWidth: '30ch' }}>
       <Form.Group>
         <Form.Label>Device Name</Form.Label>
-        <Form.Control value={deviceName} onChange={x => setDeviceName(x.target.value)} />
+        <Form.Control value={deviceName} onChange={x => setDeviceName(x.target.value)} disabled={formLock} />
       </Form.Group>
       <Form.Group>
         <Form.Label style={{ paddingTop: '10px' }}>Device EUI</Form.Label>
-        <Form.Control value={deviceEUI} onChange={x => setDeviceEUI(x.target.value)} />
+        <Form.Control value={deviceEUI} onChange={x => setDeviceEUI(x.target.value)} disabled={formLock} />
       </Form.Group>
       <div style={{ paddingTop: '10px' }}>
         <Button type="submit">Submit</Button>
       </div>
+      <RegistrationIcon status={registrationStatus} />
     </Form>
   )
+}
+
+ButtonRegistrationView.propTypes = {
+  clickupToken: PropTypes.string.isRequired,
+}
+
+function RegistrationIcon(props) {
+  const { status } = props
+
+  if (status === 'waiting') {
+    return <Spinner animation="border" />
+  }
+  if (status === 'success') {
+    return <Badge bg="success">Success</Badge>
+  }
+  if (status.includes('Error')) {
+    return <Badge bg="danger">{status}</Badge>
+  }
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return <></>
+}
+
+RegistrationIcon.propTypes = {
+  status: PropTypes.string.isRequired,
 }
