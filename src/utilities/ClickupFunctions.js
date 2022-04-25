9e80a533a71932e86e989b54ce16fbf65a228e86
Reese Critchlow <reesecritchlow@gmail.com>
2022-04-25 14:20:13 -0700

    resolved custom field bug

--- src/utilities/ClickupFunctions.js
+++ src/utilities/ClickupFunctions.js
@@ -455,7 +455,7 @@ function getCustomFieldValue(customFieldObject, customFieldID) {
   })
   console.log(desiredField)
   if (desiredField.length === 1) {
-    return desiredField.value
+    return desiredField.flat.value
   }
   return ''
 }
