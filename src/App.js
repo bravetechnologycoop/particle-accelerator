aec5584962a5da250a17c6ffc2e9d80eabfada2e
Micah Baker <micahb923@gmail.com>
2023-10-18 13:31:46 -0700

    Added Google Log In for PA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import React from 'react'
+import React, { useState } from 'react'
+import { GoogleLogin } from '@react-oauth/google'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import RouterInterface from './upper-level-components/RouterInterface'
 import Pages from './upper-level-components/Pages'
 import PageNotFound from './upper-level-components/PageNotFound'
+import BraveLogo from './pdf/BraveLogo.svg'
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+
   return (
-    // eslint-disable-next-line react/jsx-filename-extension
-    <BrowserRouter>
-      <Routes>
-        {Object.values(Pages).map(page => {
-          return page.paths.map(route => {
-            return <Route path={`${route}`} element={<RouterInterface viewState={page.displayName} />} />
-          })
-        })}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    // eslint-disable-next-line react/jsx-filename-extension, react/jsx-no-useless-fragment
+    <>
+      {loggedIn ? (
+        <BrowserRouter>
+          <Routes>
+            {Object.values(Pages).map(page => {
+              return page.paths.map(route => {
+                return <Route path={`${route}`} element={<RouterInterface viewState={page.displayName} />} />
+              })
+            })}
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        <div className="googleLoginContainer">
+          <img src={BraveLogo} alt="Brave Logo" width="128px" />
+          <h2>Please log in to use PA.</h2>
+          <GoogleLogin
+            onSuccess={response => {
+              console.log('Login Success:', JSON.stringify(response))
+              setLoggedIn(true)
+            }}
+            onError={error => {
+              console.log('Login Error:', error)
+            }}
+          />
+        </div>
+      )}
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App'
 // eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
 import reportWebVitals from './reportWebVitals'
 
+console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID)
+
 ReactDOM.render(
   // eslint-disable-next-line react/jsx-filename-extension
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>,
   // eslint-disable-next-line no-undef
   document.getElementById('root'),
 )
