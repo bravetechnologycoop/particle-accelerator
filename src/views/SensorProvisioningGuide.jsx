de8500c65173ec7d45797d1ea04a9d6a7fb0b0e7
Darrickg <darrickgunawan@gmail.com>
2024-06-06 12:08:31 -0700

    changed all the texts and images for the PA provisioning guide

--- src/views/SensorProvisioningGuide.jsx
+++ src/views/SensorProvisioningGuide.jsx
@@ -134,10 +134,13 @@ function SensorProvisioningGuide() {
                   </a>{' '}
                   to see what the highest <code>number</code> currently is.
                 </li>
+                <li>
+                  Scan the QR code on the Boron. The first string is the <code>Device Serial Number</code>.
+                </li>
               </ul>
             </div>
             <div style={styles.stepGraphic}>
-              <img src={Step4} alt="Step 4" />
+              <img src={Step3} alt="Step 3" />
             </div>
           </Card.Body>
         </Card>
@@ -153,13 +156,17 @@ function SensorProvisioningGuide() {
                 </li>
               </ol>
               <ul>
-                <li>Use the big blue USB charger.</li>
-                <li>This may take a considerable amount of time.</li>
+                <li>Attach the antenna to the boron.</li>
+                <li>Use a micro-USB cable to connect it to a power source.</li>
+                <li>Green light means that...</li>
+                <li>Purple light means that...</li>
+                <li>Cyan light means that...</li>
                 <li>Ensure that the cyan light has been breathing for at least a minute.</li>
+                <li>This step might take a considerable amount of time. The boron must be connected to power for the next steps.</li>
               </ul>
             </div>
             <div style={styles.stepGraphic}>
-              <img src={Step5} alt="Step 5" />
+              <img src={Step4} alt="Step 4" />
             </div>
           </Card.Body>
         </Card>
@@ -171,19 +178,17 @@ function SensorProvisioningGuide() {
             <div style={styles.stepText}>
               <ol start="5">
                 <li style={styles.stepTitle}>
-                  Obtain an IM21 Door Sensor and retrieve it&apos;s BLE ID (shown in image) and use the{' '}
-                  <a href="/door-sensor-pairing">Door Sensor Pairing Tool</a> to pair the IM21 to the Boron.
+                  Obtain an IM24 Door Sensor and retrieve it&apos;s BLE ID (shown in image) to pair it to the Boron.
                 </li>
               </ol>
               <ul>
-                <li>The BLE ID can be found between the two blocks of the IM21 sensor.</li>
-                <li>The Boron must be connected to power and internet.</li>
-                <li>Input the red values as a parameter as shown</li>
-                <li>Make sure the blue values match the one of the two addresses on the pictures</li>
+                <li>The BLE ID can be found at the side of the bigger block of the IM24 sensor.</li>
+                <li>Take note of the last 6 digits of the ID.</li>
               </ul>
             </div>
             <div style={styles.stepGraphic}>
-              <img src={Step6} alt="Step 6" />
+              {/* ADD IMAGE HERE */}
+              {/* <img src={Step5} alt="Step 5" /> */}
             </div>
           </Card.Body>
         </Card>
@@ -194,14 +199,17 @@ function SensorProvisioningGuide() {
           <Card.Body style={styles.cardBodyParent}>
             <div style={styles.stepText}>
               <ol start="6">
-                <li style={styles.stepTitle}>Wait for the Device to finish pairing.</li>
+                <li style={styles.stepTitle}>Open the Particle Console to the device and pair the Boron with the IM24 Door Sensor.</li>
               </ol>
               <ul>
-                <li>The Boron must be connected to power and internet.</li>
+                <li>In the top right, change Sandbox to Brave Technology Coop.</li>
+                <li>Click on Production Sensor Devices.</li>
+                <li>Click on Devices (cube on left bar) and search for the newly connected device by name.</li>
+                <li>Under functions, input in <code>Change_IM24_Door_ID</code> the last 6 digits of the IM24 ID with commas after every 2 digits.</li>
               </ul>
             </div>
             <div style={styles.stepGraphic}>
-              <img src={Step7} alt="Step 7" width="300px" />
+              <img src={Step6} alt="Step 6" width="300px" />
             </div>
           </Card.Body>
         </Card>
@@ -213,26 +221,12 @@ function SensorProvisioningGuide() {
             <div style={styles.stepText}>
               <ol start="7">
                 <li style={styles.stepTitle}>
-                  Connect the Particle to a Brave Sensor PCB and open the{' '}
-                  <a
-                    href={`https://console.particle.io/${process.env.REACT_APP_PARTICLE_SENSOR_PRODUCT_URL}/devices`}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Particle Console
-                  </a>{' '}
-                  to the device. <br />
-                  <br /> Enter <code>1</code> in the <code>Turn_Debugging_Publishes_On_Off</code> function.
+                Pass <code>1</code> to <code>Turn_Debugging_Publishes_On_Off</code> to start the event stream.
                 </li>
               </ol>
-              <ul>
-                <li>It is considered best practice to have the PCB mounted in the enclosure at this point.</li>
-                <li>Ensure that the PCB has been tested using the ESP32 testing tool.</li>
-                <li>The Boron must be connected to power and internet.</li>
-              </ul>
             </div>
             <div style={styles.stepGraphic}>
-              <img src={Step8} alt="Step 8" />
+              <img src={Step7} alt="Step 7" />
             </div>
           </Card.Body>
         </Card>
@@ -243,54 +237,39 @@ function SensorProvisioningGuide() {
           <Card.Body style={styles.cardBodyParent}>
             <div style={styles.stepText}>
               <ol start="8">
-                <li style={styles.stepTitle}>To declare the device successfully tested, wait until the following debugging values are achieved.</li>
+                <li style={styles.stepTitle}>Test the Door Sensor. To declare the device successfully tested, wait until the following debugging values are achieved.</li>
               </ol>
               <div style={{ paddingLeft: '2em' }}>
-                <code>INS_val</code> (high movement) &gt; <code>2500</code>
+
+                <code>door_status</code> (first status) = <code>0x99</code>
                 <ul>
-                  <li>Shake the Door Sensor Around and Create Movement</li>
+                  <li>Upon connection, this should be the status.</li>
                 </ul>
-                <code>INS_val</code> (low movement) &lt; <code>60</code>
+
+                <code>door_status</code> (closed door, no tamper) = <code>0x01</code>
                 <ul>
-                  <li>Leave the Door Sensor to Rest on a Table</li>
+                  <li>Join the Door Sensor Blocks and hold down the tamper at the back.</li>
                 </ul>
-                <code>door_status</code> (open door) = <code>0x02</code>
+
+                <code>door_status</code> (closed door, tamper) = <code>0x02</code>
                 <ul>
-                  <li>Separate the Door Sensor Blocks</li>
+                  <li>Join the Door Sensor Blocks and do not hold down the tamper at the back.</li>
                 </ul>
-                <code>door_status</code> (closed door) = <code>0x00</code>
+
+                <code>door_status</code> (open door, no tamper) = <code>0x03</code>
                 <ul>
-                  <li>Join the Door Sensor Blocks</li>
+                  <li>Separate the Door Sensor Blocks and hold down the tamper at the back.</li>
                 </ul>
-                Other Door Sensor Codes:
+
+                <code>door_status</code> (open door, tamper) = <code>0x04</code>
                 <ul>
-                  <li>
-                    <code>0x04</code>: Closed and Low Battery
-                  </li>
-                  <li>
-                    <code>0x08</code>: Closed and Heartbeat
-                  </li>
-                  <li>
-                    <code>0x0C</code>: Closed, Heartbeat, and Low Battery
-                  </li>
-                  <li>
-                    <code>0x06</code>: Open and Low Battery
-                  </li>
-                  <li>
-                    <code>0x0A</code>: Open and Heartbeat
-                  </li>
-                  <li>
-                    <code>0x0E</code>: Open, Heartbeat, and Low Battery
-                  </li>
+                  <li>Seperate the Door Sensor Blocks and hold down the tamper at the back.</li>
                 </ul>
+
               </div>
-              <br />
-              <ul>
-                <li>The Boron must be connected to power and internet.</li>
-              </ul>
             </div>
             <div style={styles.stepGraphic}>
-              <img src={Step9} alt="Step 9" />
+              <img src={Step8} alt="Step 8" />
             </div>
           </Card.Body>
         </Card>
@@ -305,12 +284,9 @@ function SensorProvisioningGuide() {
                   Pass <code>0</code> to <code>Turn_Debugging_Publishes_On_Off</code> to stop the event stream.
                 </li>
               </ol>
-              <ul>
-                <li>The Boron must be connected to power and internet.</li>
-              </ul>
             </div>
             <div style={styles.stepGraphic}>
-              <img src={Step10} alt="Step 10" />
+              <img src={Step9} alt="Step 9" />
             </div>
           </Card.Body>
         </Card>
@@ -322,19 +298,16 @@ function SensorProvisioningGuide() {
             <div style={styles.stepText}>
               <ol start="10">
                 <li style={styles.stepTitle}>
-                  Change the ClickUp status of the device from{' '}
-                  <mark style={{ background: ClickupStatuses.pairedDoorSensor.color }}>Paired Door Sensor</mark>
-                  to <mark style={{ background: ClickupStatuses.tested.colour }}>Tested</mark>
-                  in the{' '}
-                  <a
-                    href={`https://app.clickup.com/${process.env.REACT_APP_CLICKUP_BRAVE_TEAM_ID}/v/l/li/${process.env.REACT_APP_CLICKUP_PA_TRACKER_LIST_ID_PROD}`}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    PA Tracker.
-                  </a>
+                  With the Boron, PCB, and Antenna inside an enclosure with a Paired Door Sensor, select the current device in the{' '}
+                  <a href="/renamer">Renamer</a>
+                  <hr />
+                  Enter a unique <code>locationID</code> for the device and follow the prompts for all of the other configurations.
                 </li>
               </ol>
+            </div>
+            <div style={styles.stepGraphic}>
+              <img src={Step10} alt="Step 10" />
+              <img src={Step10_2} alt="Step 10" />
             </div>
           </Card.Body>
         </Card>
@@ -346,30 +319,9 @@ function SensorProvisioningGuide() {
             <div style={styles.stepText}>
               <ol start="11">
                 <li style={styles.stepTitle}>
-                  With the Boron, PCB, and Antenna inside an enclosure with a Paired Door Sensor, select the current device in the{' '}
-                  <a href="/renamer">Renamer</a>
-                  <hr />
-                  Enter a unique <code>locationID</code> for the device and follow the prompts for all of the other configurations.
-                </li>
-              </ol>
-            </div>
-            <div style={styles.stepGraphic}>
-              <img src={Step12} alt="Step 12" />
-              <img src={Step12_2} alt="Step 12" />
-            </div>
-          </Card.Body>
-        </Card>
-      </div>
-
-      <div style={styles.cardBoundary}>
-        <Card style={styles.cardInterior}>
-          <Card.Body style={styles.cardBodyParent}>
-            <div style={styles.stepText}>
-              <ol start="12">
-                <li style={styles.stepTitle}>
                   Connect the{' '}
-                  <a href="https://www.polono.com/pages/driver-user-manual" target="_blank" rel="noreferrer">
-                    Polono PL60 Label Printer
+                  <a href="https://support.munbyn.com/hc/en-us/articles/6092544741651-Printer-User-Manuals" target="_blank" rel="noreferrer">
+                    MUNBYN 941 USB Label Printer
                   </a>{' '}
                   to your computer and print the labels provided by the Renamer.
                 </li>
@@ -378,13 +330,13 @@ function SensorProvisioningGuide() {
                 Print the three generated labels:
                 <ul>
                   <li>One Brave Sensor Label to go on the external packaging (save for later)</li>
-                  <li>One Main Sensor Label a public-facing side of the ceiling unit enclosure</li>
+                  <li>One Main Sensor Label a public-facing side of the boron enclosure</li>
                   <li>One Door Sensor Label to go on the door sensor box</li>
                 </ul>
               </div>
             </div>
             <div style={styles.stepGraphic}>
-              <img src={Step13} alt="Step 13" />
+              <img src={Step11} alt="Step 11" />
             </div>
           </Card.Body>
         </Card>
